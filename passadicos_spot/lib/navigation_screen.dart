8f4a823d1c668442f7ca9644ef9ed2bff5265056
João Soares <joaots@ua.pt>
2020-12-15 23:43:21 +0000

    NavegationBar Done Well

--- passadicos_spot/lib/navigation_screen.dart
+++ passadicos_spot/lib/navigation_screen.dart
@@ -1,52 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:passadicos_spot/feed_screen.dart';
+import 'package:passadicos_spot/mapa_screen.dart';
 import 'package:passadicos_spot/profile_screen.dart';
+import 'package:provider/provider.dart';
 
-import 'feed_screen.dart';
-import 'mapa_screen.dart';
 
 class NavigationScreen extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Tabs Demo'),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.add_location), text: "Map"),
-                Tab(icon: Icon(Icons.article), text: "Feed"),
-                Tab(icon: Icon(Icons.account_circle), text: "Perfil")
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              MapaScreen(),
-              FeedScreen(),
-              ProfileScreen()
-            ],
-          ),
-        ),
-      ),
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider<BottomNavigationBarProvider>(
+        child: BottomNavigationBarExample(),
+        create: (BuildContext context) => BottomNavigationBarProvider(),
       ),
     );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  @override
+  _BottomNavigationBarExampleState createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  var currentTab = [
+    MapaScreen(),
+    FeedScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<BottomNavigationBarProvider>(context);
+    return Scaffold(
+      body: currentTab[provider.currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: provider.currentIndex,
+        onTap: (index) {
+          provider.currentIndex = index;
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.add_location),
+            title: new Text('Mapa'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.article),
+            title: new Text('Feed'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text('Profile'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class BottomNavigationBarProvider with ChangeNotifier {
+  int _currentIndex = 0;
+
+  get currentIndex => _currentIndex;
+
+  set currentIndex(int index) {
+    _currentIndex = index;
+    notifyListeners();
   }
 }
