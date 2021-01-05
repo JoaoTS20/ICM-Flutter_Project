f7be411d689a633b7513a1bc82a5e66344362fa6
João Soares <joaots@ua.pt>
2021-01-05 20:23:23 +0000

    Feed Visual Presentation Done

--- passadicos_spot/lib/Screens/feed_screen.dart
+++ passadicos_spot/lib/Screens/feed_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
 import 'package:passadicos_spot/Classes/Imagem.dart';
 
 class FeedScreen extends StatelessWidget {
@@ -47,7 +49,63 @@ class _FeedPageState extends State<FeedPage>{
   }
   Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     final imagem = Imagem.fromSnapshot(data);
-    return new Text(imagem.toString());
+    final l=imagem.photoURL;
+    /*String x;
+    getImage(l).then((String url){
+      setState(() {
+              x = url;
+              //Image.network(url);
+            });
+    });*/
+    return FutureBuilder<dynamic>(
+      future: getImage(l), // function where you call your api
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {  // AsyncSnapshot<Your object type>
+        if( snapshot.connectionState == ConnectionState.waiting){
+          return  Container(margin:EdgeInsets.all(8.0), child: LinearProgressIndicator());
+        }else{
+          if (snapshot.hasError)
+            return Center(child: Text('Error: ${snapshot.error}'));
+          else
+            return    Container(
+              margin:EdgeInsets.all(8.0),
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                child: InkWell(
+                  onTap: () => print("ciao"),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,  // add this
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8.0),
+                          topRight: Radius.circular(8.0),
+                        ),
+                        child: Image.network(
+                            snapshot.data,
+                            // width: 300,
+                            height: 250,
+                            fit:BoxFit.fill
+
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(imagem.username),
+                        subtitle: Text(imagem.description),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ); //:- get your object which is pass from your downloadData() function
+        }
+      },
+    );
+
+    //print("isto");
+    //print(urlImage);
+    //StorageReference _ref =   _storage.getReferenceFromUrl(l) as StorageReference;
+    //Image.network(_ref.getDownloadURL().toString());
+    //return new Text(imagem.toString());
     /*return Padding(
       key: ValueKey(imagem.username),
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
@@ -64,6 +122,13 @@ class _FeedPageState extends State<FeedPage>{
       ),
     );*/
   }
+
+}
+//Obter link para imagens Storage
+Future<dynamic> getImage(String l) async {
+  FirebaseStorage _storage = FirebaseStorage.instance;
+  StorageReference _ref =  await _storage.getReferenceFromUrl(l);
+  return _ref.getDownloadURL();
 
 }
 
